ddb57d5b26181731e906c33e9bf791eda9799856
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-03 10:20:57 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "17cac6ea-64f5-466d-a1e0-2853f14c605e",
+    "home_feed": [
+        {
+            "id": "ecf75598-f4b9-425c-a160-3b005aa83246",
+            "artist_name": "box",
+            "home_carousel": [
+                {
+                    "id": "d930686c-ef23-4317-9290-fcd43fe68fb6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/224.png"
+                },
+                {
+                    "id": "21784161-04b2-4d4f-bac4-cfe38fce24ed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png"
+                },
+                {
+                    "id": "3a01bc71-e209-4416-9951-b51d31e9ce4d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                },
+                {
+                    "id": "3b2d8f83-3cc9-42cc-92b8-030eb1b78fa0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "156b4a3e-9b95-439b-94b9-a0ab5e85ac8a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/145.png"
+                }
+            ],
+            "art_caption": "throw",
+            "art_description": "Law good type keep region sport. Here require white range move main. Turn thousand road right report."
+        },
+        {
+            "id": "38a859db-3640-404c-a0ba-1c2a0f7f62f6",
+            "artist_name": "task",
+            "home_carousel": [
+                {
+                    "id": "e23d93fb-b753-4a17-b6d2-162e8ee4d1e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                },
+                {
+                    "id": "1c139d94-4ca2-4936-ac5a-75fc90d9fed0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                },
+                {
+                    "id": "21ebf9ea-2fdf-4e67-9be2-30d3910865f9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "e74a708e-a76c-46f3-b45b-8df2c58309f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "614edbef-f440-4edf-ab72-3cbdf47dc22e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png"
+                }
+            ],
+            "art_caption": "of",
+            "art_description": "It anything effort many spring consumer administration."
+        },
+        {
+            "id": "ebdb1521-691d-4e52-a6af-e282dfe26c98",
+            "artist_name": "speak",
+            "home_carousel": [
+                {
+                    "id": "0419312c-c02f-4bd7-ad8e-68b0dad527a9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "f3197012-95dd-4882-8e62-5292c4d99b65",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png"
+                },
+                {
+                    "id": "f9cb7968-0e78-45f3-b46b-95f9df1501b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+                },
+                {
+                    "id": "3a6c0166-d01a-4fc4-90ed-b09d1beba81d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/335.png"
+                },
+                {
+                    "id": "88bca5da-94cb-4d6a-b464-0b62c9061984",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/241.png"
+                }
+            ],
+            "art_caption": "nation",
+            "art_description": "Every event unit store leave. Adult them analysis paper Mr."
+        },
+        {
+            "id": "e486a880-f6e1-4a62-9078-135f47070c35",
+            "artist_name": "probably",
+            "home_carousel": [
+                {
+                    "id": "1555a3fe-d94b-4c54-8bcc-2a5f10ba4bee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png"
+                },
+                {
+                    "id": "cfdaaeb2-d897-46bf-bf80-0ef8e6719406",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "47f4fd08-828d-4522-b388-ef48d5017215",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/226.png"
+                },
+                {
+                    "id": "c0f83875-f3fe-4fb9-a790-75d5a363ce6b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png"
+                },
+                {
+                    "id": "b559e565-46ef-4919-9e11-616507d58984",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                }
+            ],
+            "art_caption": "second",
+            "art_description": "Research likely follow indicate what approach. Author similar time clearly. Simply foot responsibility."
+        },
+        {
+            "id": "7836efec-e0da-4369-aeb9-e673bfabbeb8",
+            "artist_name": "a",
+            "home_carousel": [
+                {
+                    "id": "cd9b7cfd-8818-4c35-9014-d9ac6c2b45a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+                },
+                {
+                    "id": "c1852588-cfbc-435e-8a17-ae2d50d6354f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                },
+                {
+                    "id": "b6f4de4f-f0b9-4262-aab2-bba4222c9318",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "4cc8529e-465c-42ef-bb4d-9bcdb88df179",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/261.png"
+                },
+                {
+                    "id": "b7c5add4-6711-4cf4-9002-48576228a090",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/82.png"
+                }
+            ],
+            "art_caption": "almost",
+            "art_description": "Exist ahead political again agency. Letter recently program find check. Total certain picture though feeling."
+        }
+    ]
+}
